5d5f8ffb59066450ce3bf32c6db14cad872f6d0f
Siddharth Venu <siddharthvvenu@gmail.com>
2020-04-16 23:56:12 +0530

    Respond to requests by fetching response from one of the two URL variants

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-addEventListener("fetch", event => {
+const variantsListUrl =
+  "https://cfw-takehome.developers.workers.dev/api/variants";
+
+addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 /**
- * Respond with hello worker text
+ * Fetch the URL variants through variantsListUrl
+ */
+async function fetchUrlVariants() {
+  const response = await fetch(variantsListUrl);
+  if (response.ok) {
+    return await response.json();
+  }
+  return [];
+}
+
+/**
+ * Fetch and choose a URL variant from an array with each element
+ * having equal probability of being chosen
+ */
+async function chooseUrlVariant() {
+  const urls = (await fetchUrlVariants()).variants;
+  return urls[getRandomInt(urls.length)];
+}
+
+/**
+ * Respond with one of the two variants given by variantsListUrl
+ *
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response("Hello worker!", {
-    headers: { "content-type": "text/plain" }
-  });
+  try {
+    const urlChosen = await chooseUrlVariant();
+    return await fetch(urlChosen);
+  } catch (e) {
+    console.log(`Exception ${e}`);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
